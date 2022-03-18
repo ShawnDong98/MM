397c37a1fbc25198f7a58e8a4babf5abb911193d
ShawnDong98 <ShawnDong98@163.com>
2022-03-18 17:18:37 +0800

    LXMERT

---
+++ lxmert/data/convert_results.py
@@ -0,0 +1,24 @@
+import json
+
+with open('/home/datasets/VizWiz/Annotations/test.json', 'r') as f:
+    labels = json.load(f)
+
+with open('../snap/vizwiz/vizwiz0318_results/test_predict.json', 'r') as f:
+    oscar_results = json.load(f)
+
+outputs = []
+for i, result in enumerate(oscar_results):
+    q_id = result['question_id']
+    answer = result['answer']
+    unit = {
+            'image':labels[i]['image'],
+            'answer':answer
+            }
+    # print(unit)
+    # break
+    outputs.append(unit)
+
+with open("results.json", 'w') as f:
+    json.dump(outputs, f)
+
+
