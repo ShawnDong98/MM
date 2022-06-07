4364545f583ae133cbce4755eaf89f77ebf132de
ShawnDong98 <ShawnDong98@gmail.com>
2022-06-07 16:50:30 +0800

    vqa-maskrcnn-benchmark

---
+++ vqa-maskrcnn-benchmark/scripts/extract_features.sh
@@ -0,0 +1,5 @@
+python mmf/mmf/tools/scripts/features/extract_features_vmb.py \
+	--model_name=X-152 \
+	--image_dir=/home/datasets/VizWiz/val \
+	--output_folder=/home/datasets/VizWiz/val_features
+
